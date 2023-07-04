0b95e5bd62509ac7703a3dec5fa48dcfb96e06e8
Nick McArthur <mcarthurnick@gmail.com>
2023-07-04 10:15:36 -0600

    Completed spread labs, all tests passing.

--- spread.js
+++ spread.js
@@ -17,7 +17,7 @@ let product = {
   imageUrl: 'https://i.ebayimg.com/images/g/9T4AAOSwbmdZf2jN/s-l640.jpg',
 };
 
-// code here
+let productCopy = {...product}
 
 ////////// PROBLEM 2 //////////
 
@@ -25,7 +25,7 @@ let product = {
 
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-// code here
+let colorsCopy = [...colors]
 
 ////////// PROBLEM 3 //////////
 
@@ -37,7 +37,7 @@ function addNums(num1, num2, num3) {
   return num1 + num2 + num3;
 }
 
-// code here
+let result = addNums(...numbers);
 
 ////////// PROBLEM 4 //////////
 
@@ -46,4 +46,4 @@ function addNums(num1, num2, num3) {
 let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
-// code here
+let combinedArrays = [...arr1 , ...arr2];
